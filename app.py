2eb343e5f283c1ecc862ff242163e739e5c9c0a9
Marc Kurz <marc.kurz@fh-hagenberg.at>
2022-05-30 10:56:25 +0200

    added python file

---
+++ app.py
@@ -0,0 +1,21 @@
+
+from flask import Flask, render_template
+import random
+import platform
+
+app = Flask(__name__)
+
+#list of cat images
+images = [
+    "https://brunocapuano.files.wordpress.com/2016/06/swarmnado.gif",
+    "https://i.giphy.com/OP7kIfBat5sGY.gif"
+]
+
+@app.route('/')
+def index():
+    url = random.choice(images)
+    hostname = platform.node()
+    return render_template('index.html', url=url, hostname=hostname)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
